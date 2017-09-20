84035be4c09558c8a7cba7625ad565e35803381a
Christian Beikov <christian.beikov@gmail.com>
2017-09-20 09:03:00 +0200

    Travis tests

--- travis/build.sh
+++ travis/build.sh
@@ -3,6 +3,7 @@ git clone https://github.com/beikov/maven-surefire.git
 cd maven-surefire
 mvn -DskipTests install
 cd -
+#mvn install -DskipTests=true -Dmaven.javadoc.skip=true -Dcheckstyle.skip=true -B -V
 mvn -nsu -Dsurefire.useFile=false -Dcheckstyle.skip=true test
 
 
