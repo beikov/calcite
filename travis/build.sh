06f354954c0841e2d433c1d312cbe18cab55c55b
Christian Beikov <christian.beikov@gmail.com>
2017-09-20 08:34:33 +0200

    Travis tests

--- travis/build.sh
+++ travis/build.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
-mvn -Dsurefire.useFile=false -Dcheckstyle.skip=true test &
-pid=$!
+git clone https://github.com/beikov/maven-surefire.git
+cd maven-surefire
+mvn -DskipTests install
+cd -
+mvn -nsu -Dsurefire.useFile=false -Dcheckstyle.skip=true test
 
-minutes=0
-limit=30
-while kill -0 $! >/dev/null 2>&1; do
-echo -n -e " \b" # never leave evidences!
 
-if [ $minutes == $limit ]; then
-  for vmpid in $(jps -q); do
-    if [ ! $pid ==  $vmpid ]; then
-      jstack $vmpid
-    fi
-  done
-  exit 1;
-fi
-
-minutes=$((minutes+1))
-
-sleep 60
-done
+#pid=$!
+#
+#minutes=0
+#limit=30
+#while kill -0 $! >/dev/null 2>&1; do
+#echo -n -e " \b" # never leave evidences!
+#
+#if [ $minutes == $limit ]; then
+#  for vmpid in $(jps -q); do
+#    if [ ! $pid ==  $vmpid ]; then
+#      jstack $vmpid
+#    fi
+#  done
+#  exit 1;
+#fi
+#
+#minutes=$((minutes+1))
+#
+#sleep 60
+#done
